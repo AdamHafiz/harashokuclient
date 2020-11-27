d19b0cf7bba3a44679de9d15136d0cf143d9cadf
aliff <alifzulkifeli@gmail.com>
2020-11-27 19:49:31 +0900

    somtg

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import HarashokuNavbar from './component/HarashokuNavbar'
 import HarashokuButton from './component/HarashokuButton'
-import {Container, Row} from 'react-bootstrap'
+import { Container, Row } from 'react-bootstrap'
 import axios from 'axios'
 
-function App(){
-   
+function App() {
 
-    return ( <> 
-    <HarashokuNavbar />
-    <Row className = 'd-flex justify-content-center'>
 
-    {/* <HarashokuButton clr = 'secondary' text = 'Upload' /> */}
-    <HarashokuButton clr = 'primary' text = 'Detect' />
-    </Row>
-     </> )
+    return (<>
+        <HarashokuNavbar />
+        <Row className='d-flex justify-content-center'>
+
+            <HarashokuButton clr='secondary' text='Upload' />
+            <HarashokuButton clr='primary' text='Detect' />
+        </Row>
+    </>)
 
 }
 
